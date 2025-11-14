943db64fdb3ace3f47a5ce3f691d3f177c783951
Alex Polvi <alex@polvi.net>
2025-11-13 16:43:39 -0800

    add pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import pagefind from "astro-pagefind";
 
 export default defineConfig({
   site: 'https://infinitelogic.org',
@@ -9,5 +10,6 @@ export default defineConfig({
   },
   integrations: [
     sitemap({}),
+    pagefind()
   ],
 });
