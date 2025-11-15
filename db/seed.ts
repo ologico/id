bcc84e2c61859cfd9418c2ac9e956fb164cc7fc0
Alex Polvi <alex@polvi.net>
2025-11-15 13:11:36 -0800

    working bitwarden and ios keys

--- db/seed.ts
+++ db/seed.ts
@@ -2,6 +2,7 @@ import { db, Cred } from "astro:db";
 
 export default async function () {
   await db.insert(Cred).values([
-    { id: "x49gwpP1tKDNfIDGJTCgMFLhYkY", publicKey: "{\"x\":\"EoedexVv3Z7W79yN4pr5DdamRUPrk59LYpDhW6KsP4w\",\"y\":\"Dj3uuHVmNUT1v73mNsG0-HXjHJy0uoVbWiobN3E8MGk\"}", signCount: 0 }
+    { id: "x49gwpP1tKDNfIDGJTCgMFLhYkY", publicKey: "{\"x\":\"EoedexVv3Z7W79yN4pr5DdamRUPrk59LYpDhW6KsP4w\",\"y\":\"Dj3uuHVmNUT1v73mNsG0-HXjHJy0uoVbWiobN3E8MGk\"}", signCount: 0 },
+    { id: "RNsvcVxOTLSpN0u3_YFiOw", publicKey: "{\"x\":\"VliiIzaXaddYL5Zwzz89rR3zy8VOfG3gDrIpkNCXEqk\",\"y\":\"R71HtV7epDqwuvFJ7lrGt_0A8Af1wSrg6vox-k3CnyI\"}", signCount: 0 }
   ]);
 }
